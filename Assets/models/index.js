d6873276e1365e846c2d8535712a0a4b8f8cfbe6
thossain30 <54952959+thossain30@users.noreply.github.com>
2023-04-23 00:37:39 -0400

    Started implementing sequelize models

--- Assets/models/index.js
+++ Assets/models/index.js
@@ -6,12 +6,13 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.belongsTo(Category);
 // Categories have many Products
-
+Category.hasMany(Product);
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(ProductTag);
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product);
 module.exports = {
   Product,
   Category,
--- Assets/server.js
+++ Assets/server.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const Sequelize = require('sequelize'); 
+require("dotenv").config();
+const sequelize = new Sequelize(
+  process.env.DATABASE,
+  process.env.USER,
+  process.env.PASS, 
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+    Port: 3306
+  }
+);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
+await sequelize.sync();
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+  console.log(`App listening on port http://localhost:${PORT}`);
 });
