112f0f8c093be389a2fa9fb908203c24ceb97af9
thossain30 <54952959+thossain30@users.noreply.github.com>
2023-04-23 00:45:55 -0400

    One to many relationship b/t product and category

--- Assets/models/index.js
+++ Assets/models/index.js
@@ -5,12 +5,13 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
 Product.belongsTo(Category);
 // Categories have many Products
-Category.hasMany(Product);
+Category.hasMany(Product , {
+  foreignKey: 'category_id'
+});
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(ProductTag);
+Product.belongsToMany(Tag);
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product);
 module.exports = {
