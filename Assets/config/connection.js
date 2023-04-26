71a6e069984b43666601a8e8cf4e04910918a443
thossain30 <54952959+thossain30@users.noreply.github.com>
2023-04-25 20:31:35 -0400

    Fixed issue with connecting

--- Assets/config/connection.js
+++ Assets/config/connection.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
 
 const Sequelize = require('sequelize');
-
+require('dotenv').config();
+console.log(process.env.DB_USER)
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
--- Assets/server.js
+++ Assets/server.js
@@ -1,19 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
-const Sequelize = require('sequelize'); 
-require("dotenv").config();
-const sequelize = new Sequelize(
-  process.env.DATABASE,
-  process.env.USER,
-  process.env.PASS, 
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    Port: 3306
-  }
-);
-
+const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -23,7 +11,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-sequelize.sync();
-app.listen(PORT, () => {
-  console.log(`App listening on port http://localhost:${PORT}`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on port http://localhost:${PORT}`));
 });
